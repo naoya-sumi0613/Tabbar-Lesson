4d940cb22eda59812ad3849ad1b940f41c897a07
naoya-sumi0613 <s.naoya613pcstudy@gmail.com>
2020-08-02 11:18:03 +0900

    add style11.css script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
+//tab1以外のタブを非表示にする
 $('#tab-contents .tab[id != "tab1"]').hide();
 
 $('#tab-menu a').on('click', function() {
-	$("#tab-contents .tab").hide();
+	//タブがクリックされたとき、いったん全てのタブを非表示にする
+  $("#tab-contents .tab").hide();
+
+  //class activeを削除する
 	$("#tab-menu .active").removeClass("active");
-	$(this).addClass("active");
+
+	//クリックした要素にclass activeを追加する
+  $(this).addClass("active");
+
+  //attr()はHTML要素の属性を取得したり設定できるメソッド
+  //href属性を取得　showで取得したhref属性の中身が表示される
 	$($(this).attr("href")).show();
-	return false;
+	event.preventDefault();
 });
